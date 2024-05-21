4d1c5e45474cf621956cfe5eb2074f10ad9e18aa
Ethan Olsen <131eolsen@gmail.com>
2024-05-21 14:26:02 -0700

    feat: add tailwind, tw typography, bits-ui base styling, login hero

---
+++ frontend/src/components/LoginHero.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import CrunchyrollLoginDialog from "./CrunchyrollLoginDialog.svelte";
+	import { Button } from "bits-ui";
+
+	let open = false;
+</script>
+
+<CrunchyrollLoginDialog bind:open />
+<Button.Root
+	on:click={() => (open = true)}
+	class="inline-flex h-12 items-center justify-center rounded-input bg-dark
+  px-[21px] text-[15px] font-semibold text-background shadow-mini
+  hover:bg-dark/95 active:scale-98 active:transition-all"
+>
+	Login
+</Button.Root>
